8637b997d84f4f907939037155a41711f28a4323
g-jaswanth15 <83649057+g-jaswanth15@users.noreply.github.com>
2021-06-26 22:08:11 +0530

    Add files via upload

--- 2.js
+++ 2.js
@@ -1,5 +1,13 @@
+document.querySelector('img').style.display = "none"
+document.getElementById('try').style.display = "none"
 var canvas = document.getElementById('canvas');
+canvas.style.display = "none"
 var ctx = canvas.getContext('2d');
+
+document.getElementById('start').addEventListener('click',()=>{
+    canvas.style.display = "block"
+    document.getElementById('start').style.display = "none"
+    document.querySelector('img').style.display = "block"
 
 canvas.width = window.innerWidth -40;
 
@@ -20,15 +28,11 @@ const rectangle2 = {
     dx:5
 }
 console.log(canvas.width)
-const holes1 = {
-    x: Math.floor(Math.random()*(canvas.width-50)) ,
-    y: canvas.height/2
-}
-const holes2 = {
-    x: Math.floor(Math.random()*(canvas.width-50)),
-    y: canvas.height/4
-}
+
 function player(){
+
+    ctx.fillStyle = "pink"
+ctx.fillRect(10,canvas.height/4 + 50,canvas.width-20,(canvas.height/2 -canvas.height/4  ))
     ctx.fillStyle = 'blue';
 ctx.fillRect(rectangle.x,rectangle.y,40,40);
 ctx.fillStyle = 'black'
@@ -36,90 +40,151 @@ ctx.fillRect(rectangle1.x,rectangle1.y,canvas.width-20,50);
 
 ctx.fillStyle = 'black'
 ctx.fillRect(rectangle2.x,rectangle2.y,canvas.width-20,50);
+
+
 }
 
-console.log(holes1.x)
-    console.log(holes2.x)
 
-// class holes{
-//     constructor(x,y){
-//         this.x =x;
-//         this.y = y;
-//         this.width = 50;
-//         this.height = 50;
-//     }
+class holes{
+    constructor(x,y){
+        this.x =x;
+        this.y = y;
+        this.width = 50;
+        this.height = 50;
+    }
 
-//     draw(){
-//        ctx.beginPath()
-//        ctx.fillRect(this.x,this.y,this.width,this.height)
-//        ctx.fillStyle ="rgb(243, 241, 239)"
-//        ctx.fill()
-//     }
-// }
-// var del1 = Math.floor(Math.random() *  canvas.width-50)+100
-// console.log(del1)
-// var del2 = Math.floor(Math.random() *  canvas.width-50)+100
-// player()
+    draw(){
+       ctx.beginPath()
+       ctx.fillStyle ="rgb(243, 241, 239)"
+       ctx.fillRect(this.x,this.y,this.width,this.height)
+       ctx.fill()
+    }
+}
+let roofholes = []
+let enemy = []
+var rd = Math.random()
+console.log(rd)
+let some
+
+//  function runagain(){
+
+if(rd<0.5){
+     some=  Math.floor(rd*(canvas.width-150) + 100)
+     console.log(some)
+}
+else{
+    some=  Math.floor(rd*((canvas.width/2)-150) )
+    console.log(some)
+}
+   enemy.push( new holes(some,canvas.height/2));
+
+   enemy.push(new holes(some+400,canvas.height/2))
+   
+   enemy.push(new holes(some+700,canvas.height/2))
+   
+if(rd>0.3 && rd <0.5 ){
+    roofholes.push(new holes(some-150,canvas.height/4))
+    roofholes.push(new holes(some+200,canvas.height/4))
+    roofholes.push(new holes(some+500,canvas.height/4))
+}
+else{
+    roofholes.push(new holes(some+200,canvas.height/4))
+    roofholes.push(new holes(some+500,canvas.height/4))
+}
+
+ 
 var dist = canvas.height/2 -canvas.height/4 - 40;
 console.log(dist)
-
+    
 let animateid;
+
+
 function increment(){
+
     ctx.clearRect(0,0,canvas.width,canvas.height)
     player();
+    // runagain()
     rectangle.x += rectangle.dx;
 
-     
-    if(rectangle.x+60 > canvas.width){
-        rectangle.x = rectangle.dx
-        holes1.x =  Math.floor(Math.random()*(canvas.width-50))
-        holes2.x = Math.floor(Math.random()*(canvas.width-50))
-    }
-    
-
-    
     animateid =requestAnimationFrame(increment);
 
-     
-     ctx.fillStyle = "rgb(243, 241, 239)"
-    ctx.fillRect(holes1.x,holes1.y ,50,50)
-
-    ctx.fillStyle = "rgb(243, 241, 239)"
-    ctx.fillRect(holes2.x,holes2.y ,50,50)
-    let dist1 = holes1.x - rectangle.x -40
-    let dist2 = holes1.x - rectangle.x 
-
-    let dist3 = holes2.x - rectangle.x -40
-    let dist4 = holes2.x - rectangle.x 
-    //console.log(dist3)
-    let hi = holes1.y 
-    let pi = holes2.y +50
-    //console.log(pi)
-
+   enemy.forEach(eny =>{
+       eny.draw()
+       let dist1 = eny.x - rectangle.x -40
+    let dist2 = eny.x - rectangle.x 
+    let hi = eny.y 
     if(dist1 <1 && dist2 > -50 && rectangle.y +40 == hi){
         cancelAnimationFrame(animateid)
+        document.querySelector('img').style.display = "none"
+        document.getElementById('try').style.display = "block"
     }
+    else{
+        score += 50;
+        document.getElementById('score').innerText = score
+    }
+    let bi = eny.x
+   })
 
+roofholes.forEach(roof =>{
+    roof.draw()
+    let dist3 = roof.x - rectangle.x -40
+    let dist4 = roof.x - rectangle.x 
+    let pi = roof.y +50
     if(dist3 <1 && dist4 > -50 && rectangle.y  == pi){
         cancelAnimationFrame(animateid)
+        document.querySelector('img').style.display = "none"
+        document.getElementById('try').style.display = "block"
     }
+    if(rectangle.x > roof.x){
+        
+    }
+    let hki = roof.x
+})
+
+
+
+if(rectangle.x+60 > canvas.width){
+    rectangle.x = rectangle.dx
+    // roofholes.forEach(roof =>{
+    //     roof.x=  Math.round(Math.random()*(canvas.width-50))
+    // })
+    // enemy.forEach(eny =>{
+    //     eny.x=  Math.round(Math.random()*(canvas.width-50))
+    // }) 
+
+    rd = Math.random()
+    console.log(rd)
+    
+    if(rd<0.5){
+        some=  Math.floor(rd*(canvas.width-150) + 100)
+        console.log(some)
+   }
+   else{
+       some=  Math.floor(rd*((canvas.width/2)-150) )
+       console.log(some)
+   }
+      enemy[0].x=some
+   
+      enemy[1].x =some+400
+      
+      enemy[2].x =some+700
+      
+   if(rd>0.3 && rd <0.5 ){
+       roofholes[0].x = some-150
+       roofholes[1].x = some+200
+       roofholes[2].x = some+500
+   }
+   else{
+    roofholes[0].x = some+200
+    roofholes[1].x = some+500
+   }
+     
+}
+    
 }
 
-// holeenimies = []
-// function enimies(){
-
-//     setInterval(()=>{
-//      const x =Math.floor(Math.random() *  canvas.width-50)+100
-//      const y = canvas.height/2
-     
-//     holeenimies.push(new holes(x,y))
-//     console.log('go')
-//     },1000)
-
-// }
 increment();
-//enimies()
-
+var score =0 
 
 addEventListener('click',()=>{
     if(rectangle.y ==canvas.height/2 -40 ){
@@ -132,3 +197,15 @@ addEventListener('click',()=>{
     }
 })
 
+addEventListener('spacebar',()=>{
+    if(rectangle.y ==canvas.height/2 -40 ){
+    rectangle.y = rectangle.y-60;
+    console.log(rectangle.y)
+    }
+    else if(rectangle.y ==canvas.height/4 +50){
+          rectangle.y = rectangle.y+60;
+          console.log(rectangle.y)
+    }
+})
+
+})
