3565c76dd4d31109708ba6427247b4a787b7c847
g-jaswanth15 <83649057+g-jaswanth15@users.noreply.github.com>
2021-06-26 23:44:33 +0530

    Add files via upload

--- 2.js
+++ 2.js
@@ -1,5 +1,9 @@
 document.querySelector('img').style.display = "none"
 document.getElementById('try').style.display = "none"
+document.getElementById('reload').style.display = "none"
+document.getElementById('tryagain').style.display = "none"
+document.getElementById('score').style.display = "none"
+document.getElementById('highscore').style.display = "none"
 var canvas = document.getElementById('canvas');
 canvas.style.display = "none"
 var ctx = canvas.getContext('2d');
@@ -11,6 +15,10 @@ document.getElementById('start').addEventListener('click',()=>{
 
 canvas.width = window.innerWidth -40;
 
+document.getElementById('reload').style.display = "block"
+document.getElementById('reload').addEventListener('click',()=>{
+    window.location.reload(true)
+})
 
 const rectangle = {
     x:20,
@@ -41,9 +49,7 @@ ctx.fillRect(rectangle1.x,rectangle1.y,canvas.width-20,50);
 ctx.fillStyle = 'black'
 ctx.fillRect(rectangle2.x,rectangle2.y,canvas.width-20,50);
 
-
 }
-
 
 class holes{
     constructor(x,y){
@@ -66,8 +72,6 @@ var rd = Math.random()
 console.log(rd)
 let some
 
-//  function runagain(){
-
 if(rd<0.5){
      some=  Math.floor(rd*(canvas.width-150) + 100)
      console.log(some)
@@ -89,7 +93,7 @@ if(rd>0.3 && rd <0.5 ){
 }
 else{
     roofholes.push(new holes(some+200,canvas.height/4))
-    roofholes.push(new holes(some+500,canvas.height/4))
+    roofholes.push(new holes(some+550,canvas.height/4))
 }
 
  
@@ -98,32 +102,52 @@ console.log(dist)
     
 let animateid;
 
-
+var score = 0;
 function increment(){
 
+    
     ctx.clearRect(0,0,canvas.width,canvas.height)
     player();
     // runagain()
     rectangle.x += rectangle.dx;
-
+    score += rectangle.dx
     animateid =requestAnimationFrame(increment);
 
    enemy.forEach(eny =>{
        eny.draw()
        let dist1 = eny.x - rectangle.x -40
-    let dist2 = eny.x - rectangle.x 
-    let hi = eny.y 
+        let dist2 = eny.x - rectangle.x 
+        let hi = eny.y 
     if(dist1 <1 && dist2 > -50 && rectangle.y +40 == hi){
         cancelAnimationFrame(animateid)
         document.querySelector('img').style.display = "none"
         document.getElementById('try').style.display = "block"
+        document.getElementById('score').style.display = "block"
+        document.getElementById('highscore').style.display = "block"
+        var totalscore = score /10 ;
+        document.getElementById('score').innerText = "🎉 SCORE :" + totalscore + "🎉"
+        document.getElementById('tryagain').style.display = "block"
+        document.getElementById('tryagain').addEventListener('click',()=>{
+         window.location.reload(true)
+        })
+        canvas.style.display = "none"
+        
+        if(localStorage.getItem("best") == null){
+           localStorage.setItem("best",totalscore)
+           var highscore = localStorage.getItem("best")
+            document.getElementById('highscore').innerText = "🔥BEST :" + highscore + "🔥"
+           }
+        else if(totalscore > localStorage.getItem('best')){
+            localStorage.setItem("best",totalscore)
+            var highscore = localStorage.getItem("best")
+            document.getElementById('highscore').innerText = "🔥BEST :" + highscore + "🔥"
+        }
+         else{
+            var highscore = localStorage.getItem("best")
+        document.getElementById('highscore').innerText ="🔥BEST  :" +  highscore + "🔥"
+        }
     }
-    else{
-        score += 50;
-        document.getElementById('score').innerText = score
-    }
-    let bi = eny.x
-   })
+})
 
 roofholes.forEach(roof =>{
     roof.draw()
@@ -134,23 +158,36 @@ roofholes.forEach(roof =>{
         cancelAnimationFrame(animateid)
         document.querySelector('img').style.display = "none"
         document.getElementById('try').style.display = "block"
-    }
-    if(rectangle.x > roof.x){
-        
+        document.getElementById('score').style.display = "block"
+        document.getElementById('highscore').style.display = "block"
+        var totalscore = score /10 ;
+        document.getElementById('score').innerText = "🎉 SCORE :" + totalscore + "🎉"
+        document.getElementById('tryagain').style.display = "block"
+        document.getElementById('tryagain').addEventListener('click',()=>{
+         window.location.reload(true)
+        })
+        canvas.style.display = "none"
+        if(localStorage.getItem("best") == null){
+           localStorage.setItem("best",totalscore)
+           var highscore = localStorage.getItem("best")
+            document.getElementById('highscore').innerText = "🔥BEST :" + highscore + "🔥"
+           }
+        else if(totalscore > localStorage.getItem('best')){
+            localStorage.setItem("best",totalscore)
+            var highscore = localStorage.getItem("best")
+            document.getElementById('highscore').innerText = "🔥BEST :" + highscore + "🔥"
+        }
+         else{
+            var highscore = localStorage.getItem("best")
+        document.getElementById('highscore').innerText ="🔥BEST :" + highscore + "🔥"
+        }
     }
     let hki = roof.x
+    
 })
-
-
 
 if(rectangle.x+60 > canvas.width){
     rectangle.x = rectangle.dx
-    // roofholes.forEach(roof =>{
-    //     roof.x=  Math.round(Math.random()*(canvas.width-50))
-    // })
-    // enemy.forEach(eny =>{
-    //     eny.x=  Math.round(Math.random()*(canvas.width-50))
-    // }) 
 
     rd = Math.random()
     console.log(rd)
@@ -176,7 +213,7 @@ if(rectangle.x+60 > canvas.width){
    }
    else{
     roofholes[0].x = some+200
-    roofholes[1].x = some+500
+    roofholes[1].x = some+600
    }
      
 }
@@ -184,7 +221,7 @@ if(rectangle.x+60 > canvas.width){
 }
 
 increment();
-var score =0 
+
 
 addEventListener('click',()=>{
     if(rectangle.y ==canvas.height/2 -40 ){
@@ -197,7 +234,8 @@ addEventListener('click',()=>{
     }
 })
 
-addEventListener('spacebar',()=>{
+addEventListener('keydown',()=>{
+    
     if(rectangle.y ==canvas.height/2 -40 ){
     rectangle.y = rectangle.y-60;
     console.log(rectangle.y)
